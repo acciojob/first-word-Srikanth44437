29ef794aeeb7dcdb17a4b1fb67aff95fb1243402
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-01-12 15:52:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
-  // your code here
+  if (str === '' || !str.includes(' ')) {
+        return str;
+    }
+    return str.substring(0, str.indexOf(' '));
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
